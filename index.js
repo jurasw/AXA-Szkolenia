3ab28dd331413f6a93f51a1694b62e8a558499e6
jurasw <57913219+jurasw@users.noreply.github.com>
2020-11-30 20:01:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,69 @@
-
-   
-
-   
-   function signIn(){
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    //firebase config stuff
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      
+        const auth = firebase.auth();
+        
+        
+        function signUp(){
+            
+            var email = document.getElementById("email");
+            var password = document.getElementById("password");
+            
+            const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+            promise.catch(e => alert(e.message));
+            
+            alert("Signed Up");
+        }
+        
+        
+        
+        function signIn(){
+            
+            var email = document.getElementById("email");
+            var password = document.getElementById("password");
+            
+            const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+            promise.catch(e => alert(e.message));
+            
+            
+            
+            
+        }
+        
+        
+        function signOut(){
+            
+            auth.signOut();
+            alert("Signed Out");
+            
+        }
+        
+        
+        
+        auth.onAuthStateChanged(function(user){
+            
+            if(user){
+                
+                var email = user.email;
+                alert("Active User " + email);
+                
+                //Take user to a different or home page
+                window.location.href = 'https://jurasw.github.io/AXA-Szkolenia/users';
+                //is signed in
+                
+            }else{
+                
+                alert("No Active User");
+                //no user is signed in
+            }
+            
+            
+            
+        });
+        
     
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
     
-    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
-    
-    
-    
-    
-   }
-   
-   
-   function signOut(){
-    
-    auth.signOut();
-    alert("Signed Out");
-    
-   }
-   
-
-   
-   auth.onAuthStateChanged(function(user){
-    
-    if(user){
-     
-     var email = user.email;
-     //alert("Active User " + email);
-     
-     //Take user to a different or home page
-     window.location.href = "https://jurasw.github.io/platforma/profile";
-     //is signed in
-     
-    }else{
-        if (event.keyCode === 13) { 
-            signIn(); 
-        } 
-     //alert("No Active User");
-     //no user is signed in
-    }
-    
-    
-    
-   });
-
